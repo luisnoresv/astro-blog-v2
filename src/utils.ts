8ad60601f998af03e2917fea5950c7f52204e601
Luis Alberto Nores Villaverde <luisnoresv@outlook.com>
2024-03-05 18:27:46 -0500

    feat(app): add tags, cards, show content from md

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+// * Format the date to a locale string
 export function formatDate(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
@@ -6,4 +7,12 @@ export function formatDate(date: Date): string {
   };
 
   return new Date(date).toLocaleDateString(undefined, options);
+}
+
+// * Capitalize the first letter
+export function capitalize(expression: string): string {
+  if (typeof expression !== "string" || expression.length === 0) {
+    return expression;
+  }
+  return `${expression.charAt(0).toUpperCase()}${expression.slice(1)}`;
 }
