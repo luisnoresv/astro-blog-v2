79a53feb40162c58a48be42c66763791d8cb7e4b
Luis Alberto Nores Villaverde <luisnoresv@outlook.com>
2024-03-05 00:04:23 -0500

    feat(app): add static assets, components

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "TechPeople Blog";
+export const SITE_DESCRIPTION = "Articles, Stories & Tutorials For Tech People";
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
